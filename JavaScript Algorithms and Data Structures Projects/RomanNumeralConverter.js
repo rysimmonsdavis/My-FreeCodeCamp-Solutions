313a9d901d88d2f2ad1889b8bbffaef95196f8e6
N. Ry Simmons-Davis <88639333+rysimmonsdavis@users.noreply.github.com>
2022-05-09 23:28:35 -0700

    Adding descriptions to the files so far

--- JavaScript Algorithms and Data Structures Projects/RomanNumeralConverter.js
+++ JavaScript Algorithms and Data Structures Projects/RomanNumeralConverter.js
@@ -1,3 +1,10 @@
+/*
+Roman Numeral Converter
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+*/
+
 function convertToRoman(num) {
     let string = "";
     
