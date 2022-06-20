7a3924e113aae3f26702cd7459cc730e50a33214
Nathan Ry Simmons-Davis <nrsimmonsdavis@gmail.com>
2022-06-19 21:03:29 -0700

    latest solution

---
+++ Drop it.js
@@ -0,0 +1,20 @@
+/*
+Drop it
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+*/
+
+function dropElements(arr, func) {
+
+    while (func(arr[0]) == false && arr.length > 0){
+
+        arr.splice(0, 1);
+    
+    }
+
+    return arr;
+  }
+  
+  dropElements([1, 2, 3], function(n) {return n < 3; });
